202c835c6e2a918f03dbe4cee43aa11615ccbfb7
Xpektra <xpektra7@gmail.com>
2025-08-08 20:59:50 +0100

    added useNavigation to Login.jsx to disable submit button while signing in

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,26 @@
 import { useState } from "react";
-import { useLoaderData, Form, redirect } from "react-router-dom";
+import { useLoaderData, Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { login } from "../api";
 
 export async function loginLoader({ request }){
     return new URL(request.url).searchParams.get("message")
 }
 
+function sleep(ms){
+    return new Promise(resolve => setTimeout(resolve,ms))
+}
+
 export async function action({ request }){
+    await sleep(2000)
     const formData = await request.formData()
     const email = formData.get("email")
     const password = formData.get("password")
-    const data = await login({email, password})
-    return redirect("/host")
+    try {
+        const data = await login({email, password})
+        return redirect("/host")
+    } catch (error) {
+        return error
+    }
     
 } 
 
@@ -19,8 +28,8 @@ export default function Login(){
 
     const message = useLoaderData();
 
-    const [status,setStatus] = useState("idle")
-    const [error,setError] = useState(null)
+    const status = useNavigation().state
+    const error = useActionData()
 
 
     return(
@@ -41,7 +50,7 @@ export default function Login(){
                 <input type="password" name="password" id="password" className="w-full md:w-[50vw] max-w-[540px] bg-whie p-1 border border-orange-500 outline-none" placeholder="Password" required/>
                 <button type="submit" disabled={status === "submitting"} className="p-2 flex flex-col w-full md:w-[50vw] max-w-[540px] items-center  clip-van text-white bg-neutral-900 hover:bg-orange-500 transition ease-in duration-300 cursor-pointer">
                     {status === "submitting" ? "Signing in..." : "Sign in" }
-                    </button>
+                </button>
             </Form>
             <div className="flex flex-col items-center gap-2">
                 <p className="">Don't have an account? <span className="text-orange-500">Create new one</span></p>
